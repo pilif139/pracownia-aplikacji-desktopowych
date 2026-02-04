1bc80a4df41bfffe4beae155ed5d4bc6f8ebbb8d
pilif139 <pifilif@gmail.com>
2026-02-04 15:33:41 +0100

    kontynuacja tutoriala pyqt6, wstep do matlib (trzeba zupdatowac wersje pythona w systemie)

---
+++ 28_01_2026/app.py
@@ -0,0 +1,115 @@
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QMenu,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMouseTracking(True)
+        self.label = QLabel(self)
+        self.label.setMouseTracking(True)
+        self.input = QLineEdit(self)
+        self.input.textChanged.connect(self.label.setText)
+
+        self.button_is_checked = True
+
+        self.setWindowTitle("Hello World!")
+        self.button = QPushButton("Wcisnij mnie!")
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.the_button_was_clicked)
+        self.button.clicked.connect(self.the_button_was_toggled)
+        self.button.setChecked(self.button_is_checked)
+
+        self.setFixedSize(QSize(400, 300))
+        self.setMinimumSize(QSize(100, 100))
+        self.setMaximumSize(QSize(800, 600))
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.label)
+        layout.addWidget(self.input)
+        layout.addWidget(self.button)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+    def the_button_was_clicked(self):
+        self.button.setText("You clicked me!")
+        self.button.setEnabled(False)
+
+    def the_button_was_toggled(self, checked):
+        self.button_is_checked = checked
+        if checked:
+            print("Button is checked")
+        else:
+            print("Button is unchecked")
+
+    def mouseMoveEvent(self, a0):
+        self.label.setText("mouseMoveEvent")
+
+    def mousePressEvent(self, a0):
+        if a0 is None:
+            return
+        if a0.button() == Qt.MouseButton.LeftButton:
+            # handle the left-button press in here
+            self.label.setText("mousePressEvent LEFT")
+        elif a0.button() == Qt.MouseButton.MiddleButton:
+            # handle the middle-button press in here.
+            self.label.setText("mousePressEvent MIDDLE")
+        elif a0.button() == Qt.MouseButton.RightButton:
+            # handle the right-button press in here.
+            self.label.setText("mousePressEvent RIGHT")
+
+    def mouseReleaseEvent(self, a0):
+        if a0 is None:
+            return
+        if a0.button() == Qt.MouseButton.LeftButton:
+            self.label.setText("mouseReleaseEvent LEFT")
+
+        elif a0.button() == Qt.MouseButton.MiddleButton:
+            self.label.setText("mouseReleaseEvent MIDDLE")
+
+        elif a0.button() == Qt.MouseButton.RightButton:
+            self.label.setText("mouseReleaseEvent RIGHT")
+
+    def mouseDoubleClickEvent(self, a0):
+        if a0 is None:
+            return
+        if a0.button() == Qt.MouseButton.LeftButton:
+            self.label.setText("mouseDoubleClickEvent LEFT")
+
+        elif a0.button() == Qt.MouseButton.MiddleButton:
+            self.label.setText("mouseDoubleClickEvent MIDDLE")
+
+        elif a0.button() == Qt.MouseButton.RightButton:
+            self.label.setText("mouseDoubleClickEvent RIGHT")
+
+    def contextMenuEvent(self, event) -> None:
+        if event is None:
+            return
+        context = QMenu(self)
+        context.addAction(QAction("Action 1", self))
+        context.addAction(QAction("Action 2", self))
+        context.exec(event.globalPos())
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
---
+++ 28_01_2026/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 28-01-2026!")
+
+
+if __name__ == "__main__":
+    main()
